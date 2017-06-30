a8de35d3b4e5f0c26a5cda1756e7cbfd87156ecd
NajibOsman <machidydemajka38@gmail.com>
2017-06-30 14:53:35 +0300

    Added word_count.rb file to the lib directory

---
+++ lib/word_count.rb
@@ -0,0 +1,12 @@
+class String
+  define_method(:count) do |word|
+    sentence = self.split()
+    counter = 0
+    sentence.each() do |list|
+      if list == word
+        counter = counter + 1
+      end
+      end
+      counter
+  end
+end
