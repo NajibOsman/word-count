1f5e9d4ff106dbe059eaa56a4d16198667d21a1f
NajibOsman <machidydemajka38@gmail.com>
2017-07-21 17:31:21 +0300

    Included specs for word counter.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,5 +3,16 @@ require('word_count')
 
 describe('word_count') do
   it ("counts the number of times a word is repeated") do
+    expect("one".word_count()).to(eq(true))
   end
+
+  it("check if string present in sentence") do
+    expect("ruby specs one O one".word_count()).to(eq(true))
+  end
+
+  it("returns number of times a word appears in a sentence") do
+    expect("ruby specs one O one".word_count()).to(eq(2))
+  end
+
+
 end
